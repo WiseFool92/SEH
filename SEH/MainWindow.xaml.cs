d2708c103791d7304b5f8c2bf2d311df87e91884
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-10-15 11:05:46 -0700

    Remove extra btn

--- SEH/MainWindow.xaml.cs
+++ SEH/MainWindow.xaml.cs
@@ -25,11 +25,6 @@ namespace SEH
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
